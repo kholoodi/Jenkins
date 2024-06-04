a603135c6de8aa6271748d04c2717d5824cee7b8
kholoodi <kholood-2016@outlook.com>
2024-06-04 11:30:56 +0300

    Add files

---
+++ DuplicationApp/src/test/java/com/Duplication/DuplicationApp/DuplicationAppApplicationTests.java
@@ -0,0 +1,19 @@
+package com.Duplication.DuplicationApp;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuplicationAppApplicationTests {
+
+	private DuplicationAppApplication duplicationTest = new DuplicationAppApplication();
+	  @Test
+	  Public void testDoublication()
+	  {
+		  Integer actual = duplicationTest.Duplication(6);
+		  Integer expected = 12;
+		   assertEquals(expected, actual);
+                }
+
+}
+
